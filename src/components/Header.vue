de114f17282b26004196199eac4495e6c4d4fc7a
Lieselot Geirnaert <lieselot.geirnaert@studetn.odisee.be>
2018-10-05 23:53:34 +0200

    responsive menu wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,30 @@
     <header>
         <div class="navigation">
             <div class="navbar">
-                <ul class="nav-tabs">
-                    <li><a href="#about">about</a></li>
-                    <li><a href="#education">eduction</a></li>
-                    <li><a href="#it">IT knowledge</a></li>
-                    <li><a href="#experiences">experiences</a></li>
-                </ul>
+                <div class="navbar--full">
+                    <ul class="nav-tabs">
+                        <li><a href="#about">about</a></li>
+                        <li><a href="#education">eduction</a></li>
+                        <li><a href="#it">IT knowledge</a></li>
+                        <li><a href="#experiences">experiences</a></li>
+                    </ul>
+                </div>
+                <div class="navbar--mobile">
+                    <div v-on:click="change" :class="{'hamburger': !clicked, 'change hamburger': clicked }">
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </div>
+                    <ul v-if="clicked" class="nav-tabs">
+                        <li><a href="#about">about</a></li>
+                        <li><a href="#education">eduction</a></li>
+                        <li><a href="#it">IT knowledge</a></li>
+                        <li><a href="#experiences">experiences</a></li>
+                    </ul>
+                </div>
+                
             </div>
+            
         </div>
         <div class="intro">
             <h1>Lieselot</h1><h1>Geirnaert</h1>
@@ -17,3 +34,18 @@
         </div>
     </header>
 </template>
+
+<script>
+export default {
+    data () {
+        return {
+            clicked: false
+        }
+    },
+    methods: {
+        change () {
+            this.clicked = !this.clicked;
+        }
+    }
+}
+</script>
