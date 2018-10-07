27a04635eca09cf8ff77bd4f8fcea1502bea787e
Lieselot Geirnaert <lieselot.geirnaert@studetn.odisee.be>
2018-10-07 11:22:46 +0200

    responsive nav

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,5 @@
 <template>
     <div id="download">
-        <div>
-            <p>Download resume</p>
-            <a href="../assets/img/lieselot_geirnaert.jpg" target="_blank"><font-awesome-icon icon="download" />English resume</a>
-            <a href="../assets/LieselotGeirnaertCV2018.pdf" download="CVLieselotGeirnaert.pdf"><font-awesome-icon icon="download" />Dutch resume</a>
-        </div>
+        <p>&copy; Lieselot Geirnaert 2018</p>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faIdCard, faBirthdayCake, faAt, faMobileAlt, faHome, faCircle, faLocationArrow, faGlobe, faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faIdCard, faBirthdayCake, faAt, faMobileAlt, faHome, faCircle, faLocationArrow, faGlobe, faDownload, faCopyright } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faCheckCircle } from '@fortawesome/fontawesome-free-regular'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faIdCard, faBirthdayCake, faAt, faMobileAlt, faHome, faCircle, faLocationArrow, faGlobe, faDownload, faGithub, faLinkedin, faCheckCircle)
+library.add(faIdCard, faBirthdayCake, faAt, faMobileAlt, faHome, faCircle, faLocationArrow, faGlobe, faDownload, faGithub, faLinkedin, faCheckCircle, faCopyright)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
